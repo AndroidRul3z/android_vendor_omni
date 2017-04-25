3c1aa17bf60b7f57d55c4620d1c557817c10653a
AndroidRul3z <ferrettidario@gmail.com>
2017-04-25 20:23:47 +0200

    Drop omni audio files

--- config/common.mk
+++ config/common.mk
@@ -53,12 +53,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# custom sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=omni_ringtone1.ogg \
-    ro.config.notification_sound=omni_notification1.ogg \
-    ro.config.alarm_alert=omni_alarm1.ogg
-
 # Custom off-mode charger
 ifneq ($(WITH_CM_CHARGER),false)
 PRODUCT_PACKAGES += \
@@ -67,20 +61,6 @@ PRODUCT_PACKAGES += \
     font_log.png \
     libhealthd.cm
 endif
-
-PRODUCT_COPY_FILES += \
-    vendor/purity/prebuilt/sounds/camera_click_48k.ogg:system/media/audio/ui/camera_click.ogg \
-    vendor/purity/prebuilt/sounds/VideoRecord_48k.ogg:system/media/audio/ui/VideoRecord.ogg \
-    vendor/purity/prebuilt/sounds/VideoStop_48k.ogg:system/media/audio/ui/VideoStop.ogg \
-    vendor/purity/prebuilt/sounds/omni_ringtone1.ogg:system/media/audio/ringtones/omni_ringtone1.ogg \
-    vendor/purity/prebuilt/sounds/omni_ringtone2.ogg:system/media/audio/ringtones/omni_ringtone2.ogg \
-    vendor/purity/prebuilt/sounds/omni_ringtone3.ogg:system/media/audio/ringtones/omni_ringtone3.ogg \
-    vendor/purity/prebuilt/sounds/omni_alarm1.ogg:system/media/audio/alarms/omni_alarm1.ogg \
-    vendor/purity/prebuilt/sounds/omni_alarm2.ogg:system/media/audio/alarms/omni_alarm2.ogg \
-    vendor/purity/prebuilt/sounds/omni_notification1.ogg:system/media/audio/notifications/omni_notification1.ogg \
-    vendor/purity/prebuilt/sounds/omni_lowbattery1.ogg:system/media/audio/ui/omni_lowbattery1.ogg \
-    vendor/purity/prebuilt/sounds/omni_lock_phone.ogg:system/media/audio/ui/omni_lock_phone.ogg \
-    vendor/purity/prebuilt/sounds/omni_unlock_phone.ogg:system/media/audio/ui/omni_unlock_phone.ogg
 
 # Additional packages
 -include vendor/purity/config/packages.mk
