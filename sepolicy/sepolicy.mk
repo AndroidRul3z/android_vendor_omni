ef472e606ba0ed7fc3b88fe8d2890d33a405a688
AndroidRul3z <ferrettidario@gmail.com>
2017-05-15 13:40:48 +0200

    Update

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,7 +1,7 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Omni
+# inherit from Purity
 #
 
 BOARD_SEPOLICY_DIRS += \
-    vendor/omni/sepolicy
+    vendor/purity/sepolicy
